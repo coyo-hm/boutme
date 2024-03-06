31fb6e429f943f671e5741a2ddd18328e7ef11ce
COYO <dayeon.yoon@gmail.com>
2024-03-07 04:31:05 +0900

    feat: abouts

---
+++ src/components/Abouts/index.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+import styles from "./abouts.module.scss";
+import IconCalender from "@assets/calender.svg";
+import IconMail from "@assets/mail.svg";
+import IconBlog from "@assets/blog.svg";
+import IconGithub from "@assets/github.svg";
+
+export default function Abouts() {
+	return (
+		<div id={"abouts"} className={styles.abouts}>
+			<div>
+				<h1 className={styles.title}>About me</h1>
+				<div className={styles.birth}>
+					<IconCalender width={24} height={24} />
+					<span>1998</span>
+				</div>
+				<Link className={styles.mail} href="mailto:bsydwp@gmail.com">
+					<IconMail width={24} height={24} />
+					<span>bsydwp@gmail.com</span>
+				</Link>
+				<Link className={styles.link1} href={"https://coyo-hm.github.io/"} target={"_blank"} rel="noreferrer">
+					<IconBlog width={24} height={24} />
+					<span>BLOG</span>
+				</Link>
+				<Link className={styles.link2} href={"https://github.com/coyo-hm"} target={"_blank"} rel="noreferrer">
+					<IconGithub width={24} height={24} className={"text-[#667eea]"} />
+					<span>GITHUB</span>
+				</Link>
+				<ul className={styles.description}>
+					<li>FE 분야 뿐만 아니라 다양한 개발 분야를 깊이 있게 다져나가려고 노력하고 있습니다.</li>
+					<li><span className={"text-yellow-500 text-xl font-extrabold"}>사용자의 관점에서 UI를 고려하며 개선</span>하는 것을 좋아합니다. </li>
+					<li>코드의 재사용성을 높이는 것을 좋아합니다.</li>
+					<li>지속적으로 새로운 활동을 통해 발전해나가고 있습니다.</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
