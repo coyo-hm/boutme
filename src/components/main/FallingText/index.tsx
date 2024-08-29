292e6553e5ceccb958c3f9ca87388b27d0515979
COYO <dayeon.yoon@gmail.com>
2024-08-30 03:45:19 +0900

    fix(home): FallingText 앞부분 잘리는 오류 수정

--- src/components/main/FallingText/index.tsx
+++ src/components/main/FallingText/index.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useParams } from "next/navigation";
 import { useEffect, useRef } from "react";
 import Matter from "matter-js";
 import styles from "./FallingText.module.scss";
+import { Link } from "@i18n/navigation";
 
 interface Props {
 	text: string;
@@ -11,30 +11,22 @@ interface Props {
 }
 
 const FallingText = ({ text, highlightTexts = [] }: Props) => {
-	const { locale } = useParams() as { locale: string };
 	const containerRef = useRef<HTMLDivElement>(null);
-	const textRef = useRef<HTMLDivElement>(null);
+	const engineRef = useRef(Matter.Engine.create());
+	const requestRef = useRef<any>();
 
-	const splitWords = () => {
-		const textNode = textRef.current as HTMLElement;
-		const words = text.split(" ");
-		const newDomElements = words.map(word => {
-			for (const { label, url } of highlightTexts) {
-				if (word.startsWith(label)) {
-					return `<a class="${styles.word} ${styles.highlight}" href="/${locale}${url}">
-							${word}&nbsp;
-						</a>`;
-				}
-			}
-			return `<span class="${styles.word}">${word}&nbsp;</span>`;
-		});
-		textNode.innerHTML = newDomElements.join("");
-	};
+	const animate = () => {
+		const { Engine, Bodies, Composite, MouseConstraint } = Matter;
+		const container = containerRef.current as HTMLDivElement;
 
-	useEffect(() => {
-		splitWords();
+		const canvasSize = {
+			width: containerRef?.current?.clientWidth || 0,
+			height: containerRef?.current?.clientHeight || 0
+		};
 
-		const { Engine, Render, World, Bodies, Runner, Mouse, MouseConstraint } = Matter;
+		engineRef.current = Engine.create();
+		const engine = Engine.create();
+
 		const params = {
 			isStatic: true,
 			render: {
@@ -42,50 +34,38 @@ const FallingText = ({ text, highlightTexts = [] }: Props) => {
 			}
 		};
 
-		const canvasSize = {
-			width: containerRef?.current?.clientWidth || window.innerWidth - 20,
-			height: containerRef?.current?.clientHeight || window.innerHeight - 154
-		};
-		const engine = Engine.create({});
-		const render = Render.create({
-			element: containerRef.current as HTMLElement,
-			engine: engine,
-			options: {
-				...canvasSize,
-				background: "transparent",
-				wireframes: false
-			}
-		});
+		const walls = [
+			Bodies.rectangle(canvasSize.width / 2, canvasSize.height, canvasSize.width, 50, params),
+			Bodies.rectangle(0, canvasSize.height / 2, 50, canvasSize.height, params),
+			Bodies.rectangle(canvasSize.width, canvasSize.height / 2, 50, canvasSize.height, params)
+		];
 
-		const floor = Bodies.rectangle(canvasSize.width / 2, canvasSize.height, canvasSize.width, 50, params);
-		const wall1 = Bodies.rectangle(0, canvasSize.height / 2, 50, canvasSize.height, params);
-		const wall2 = Bodies.rectangle(canvasSize.width, canvasSize.height / 2, 50, canvasSize.height, params);
-		const top = Bodies.rectangle(canvasSize.width / 2, 0, canvasSize.width, 50, params);
-
-		const wordElements = document.querySelectorAll(`.${styles.word}`);
-		const wordBodies = Array.from(wordElements).map(elemRef => {
-			const width = (elemRef as HTMLElement).offsetWidth;
-			const height = (elemRef as HTMLElement).offsetHeight;
-
+		const wordElements = Array.from(container.querySelectorAll(`.${styles.word}`)) as HTMLElement[];
+		const wordBodies = wordElements.map(element => {
+			const width = element.offsetWidth;
+			const height = element.offsetHeight;
 			return {
-				body: Bodies.rectangle(canvasSize.width / 2, 0, width, height, {
+				body: Bodies.rectangle(canvasSize.width / 2, -50, width, height, {
 					render: {
 						fillStyle: "transparent"
-					}
+					},
+					restitution: 0.2
 				}),
-				elem: elemRef,
+				elem: element,
 				render() {
 					const { x, y } = this.body.position;
-					(this.elem as HTMLElement).style.top = `${y - 20}px`;
-					(this.elem as HTMLElement).style.left = `${x - width / 2}px`;
-					(this.elem as HTMLElement).style.transform = `rotate(${this.body.angle}rad)`;
+					if (y - height / 2 < 0) {
+						console.log(y, height, element);
+					}
+
+					this.elem.style.top = `${y - height / 2}px`;
+					this.elem.style.left = `${x - width / 2}px`;
+					this.elem.style.transform = `rotate(${this.body.angle}rad)`;
 				}
 			};
 		});
 
-		const mouse = Mouse.create(containerRef.current as HTMLElement);
 		const mouseConstraint = MouseConstraint.create(engine, {
-			mouse,
 			constraint: {
 				stiffness: 0.2,
 				render: {
@@ -94,32 +74,43 @@ const FallingText = ({ text, highlightTexts = [] }: Props) => {
 			}
 		});
 
-		World.add(engine.world, [floor, ...wordBodies.map(box => box.body), wall1, wall2, top, mouseConstraint]);
-
-		Runner.run(engine);
-		Render.run(render);
+		Composite.add(engine.world, [...walls, mouseConstraint, ...wordBodies.map(ele => ele.body)]);
 
 		(function rerender() {
 			wordBodies.forEach(element => {
 				element.render();
 			});
-			Matter.Engine.update(engine);
-			requestAnimationFrame(rerender);
+			Engine.update(engine);
+			requestRef.current = requestAnimationFrame(rerender);
 		})();
+	};
 
+	useEffect(() => {
+		animate();
 		return () => {
-			World.clear(engine.world, false);
-			Engine.clear(engine);
-			render.canvas.remove();
+			cancelAnimationFrame(requestRef.current);
+			Matter.Engine.clear(engineRef.current);
 		};
 	}, []);
 
 	return (
 		<div className={styles.wordContainer} ref={containerRef}>
-			<div ref={textRef}>
-				{/*<div className={"text"}>*/}
-				{text}
-			</div>
+			{text.split(" ").map((word, index) => {
+				for (const { label, url } of highlightTexts) {
+					if (word.startsWith(label)) {
+						return (
+							<Link href={url} className={`${styles.word} ${styles.highlight}`} key={index}>
+								{word}&nbsp;
+							</Link>
+						);
+					}
+				}
+				return (
+					<span className={styles.word} key={index}>
+						{word}&nbsp;
+					</span>
+				);
+			})}
 		</div>
 	);
 };
