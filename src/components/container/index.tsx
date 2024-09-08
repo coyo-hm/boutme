e588b53e993480ea7bd637fc0da3a2893a78ffc2
COYO <dayeon.yoon@gmail.com>
2024-09-09 02:18:24 +0900

    feat(container): sidebar

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -2,22 +2,16 @@
 
 import { PropsWithChildren } from "react";
 import styles from "./container.module.scss";
-import { usePathname } from "@i18n/navigation";
-import LocaleSelectBox from "@components/container/LocaleSelectBox";
-import TableOfContents from "@components/container/TableOfContents";
-import Footer from "@components/container/Footer";
+import Sidebar from "@components/container/Sidebar";
 
 interface Props extends PropsWithChildren {}
 
 const Container = ({ children }: Props) => {
-	const pathname = usePathname();
 
 	return (
 		<div className={styles.container}>
-			<LocaleSelectBox/>
-			{pathname !== "/" && <TableOfContents />}
+			<Sidebar />
 			<div className={styles.content}>{children}</div>
-			<Footer />
 		</div>
 	);
 };
