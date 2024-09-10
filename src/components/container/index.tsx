9b76a91767b693bf60dba0f7381747fe830ecd73
COYO <dayeon.yoon@gmail.com>
2024-09-10 14:48:45 +0900

    feat: cursor animation 주석 해제

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,29 +1,29 @@
 "use client";
 
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect, useRef } from "react";
 import styles from "./container.module.scss";
 import Sidebar from "@components/container/Sidebar";
 
 interface Props extends PropsWithChildren {}
 
 const Container = ({ children }: Props) => {
-	// const cursorRef = useRef<HTMLDivElement>(null);
-	//
-	// useEffect(() => {
-	// 	const cursor = cursorRef.current as HTMLDivElement;
-	// 	const moveCursor = (e: any) => {
-	// 		cursor.style.left = e?.pageX + "px";
-	// 		cursor.style.top = e.pageY - scrollY + "px";
-	// 	};
-	// 	window.addEventListener("scroll", moveCursor);
-	// 	window.addEventListener("mousemove", moveCursor);
-	//
-	//
-	// }, []);
+	const cursorRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const cursor = cursorRef.current as HTMLDivElement;
+		const moveCursor = (e: any) => {
+			cursor.style.left = e?.pageX + "px";
+			cursor.style.top = e.pageY - scrollY + "px";
+		};
+		window.addEventListener("scroll", moveCursor);
+		window.addEventListener("mousemove", moveCursor);
+
+
+	}, []);
 
 	return (
 		<div className={styles.container}>
-			{/*<div className={styles.cursor} ref={cursorRef} />*/}
+			<div className={styles.cursor} ref={cursorRef} />
 			<Sidebar />
 			<div className={styles.content}>{children}</div>
 		</div>
